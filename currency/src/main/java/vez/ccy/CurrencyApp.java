0ddab2f9810438dfc0b43c7c3b2a9dd579c757b0
vzateychuk <vzateychuk@gmail.com>
2024-03-03 00:40:38 +0300

    added currency module

---
+++ currency/src/main/java/vez/ccy/CurrencyApp.java
@@ -0,0 +1,13 @@
+package vez.ccy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyApp.class, args);
+    }
+
+}
